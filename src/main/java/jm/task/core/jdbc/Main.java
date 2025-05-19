611813a75378d338d9986dc6ebc4fc42ffcfc67d
fifi <suzdaltsevaas@gmail.com>
2025-05-19 11:41:17 +0400

    Внесены исправления в соответствии с третьими правками

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,18 +1,36 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.dao.DAOController;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserService service = new UserServiceImpl();
-        service.createUsersTable();
-        service.saveUser("Danil", "Chernov", (byte) 26);
-        service.saveUser("Matvey", "Perestoronin", (byte) 21);
-        service.saveUser("Leonid", "Malinov", (byte) 19);
-        service.saveUser("Anfisa", "Suzdaltseva", (byte) 27);
-        service.getAllUsers();
-        service.cleanUsersTable();
-        service.dropUsersTable();
+        UserService userService = injectUserService();
+
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Petrov", (byte) 28);
+        userService.saveUser("Danil", "Chernov", (byte) 26);
+        userService.saveUser("Matvey", "Perestoronin", (byte) 21);
+        userService.saveUser("Leonid", "Malinov", (byte) 19);
+        userService.getAllUsers().forEach(System.out::println);
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
+
+    private static UserService injectUserService() {
+        return new UserServiceImpl(DAOController.getUserDao());
+    }
+
+//        UserService service = new UserServiceImpl();
+//        service.createUsersTable();
+//        service.saveUser("Danil", "Chernov", (byte) 26);
+//        service.saveUser("Matvey", "Perestoronin", (byte) 21);
+//        service.saveUser("Leonid", "Malinov", (byte) 19);
+//        service.saveUser("Anfisa", "Suzdaltseva", (byte) 27);
+//        service.getAllUsers();
+//        service.cleanUsersTable();
+//        service.dropUsersTable();
+
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,58 +1,39 @@
 package jm.task.core.jdbc.service;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
-import jm.task.core.jdbc.model.User;
-import org.jboss.logging.Logger;
 
-import java.io.InputStream;
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.model.User;
+
 import java.util.List;
-import java.util.Properties;
 
 public class UserServiceImpl implements UserService {
-    private static final Logger logger = Logger.getLogger(UserServiceImpl.class);
-    private final static Properties propOfCon = new Properties();
+    private final UserDao userDao;
 
-    static {
-        try (InputStream input = UserServiceImpl.class.getClassLoader()
-                .getResourceAsStream("controller.properties")) {
-            propOfCon.load(input);
-        } catch (Exception e) {
-            logger.warn("Failed to load controller.properties during static initialization", e);
-        }
-    }
-
-    public static UserDao getUserDao() {
-        String type = propOfCon.getProperty("dao.impl");
-        if (type.equals("jdbc")) {
-            return new UserDaoJDBCImpl();
-        } else {
-            return new UserDaoHibernateImpl();
-        }
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
     }
 
     public void createUsersTable() {
-        getUserDao().createUsersTable();
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-        getUserDao().dropUsersTable();
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        getUserDao().saveUser(name, lastName, age);
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        getUserDao().removeUserById(id);
+        userDao.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return getUserDao().getAllUsers();
+        return userDao.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        getUserDao().cleanUsersTable();
+        userDao.cleanUsersTable();
     }
 }
